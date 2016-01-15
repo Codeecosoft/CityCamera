f45d520521a264c52e7340a612f80325c352918d
Dusan Jugovic <djugovic@dusans-mac-mini.local>
2016-01-15 18:49:33 +0100

    Initial Commit

---
+++ CityCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CityCamera
+//
+//  Created by Dusan Jugovic on 1/11/16.
+//  Copyright © 2016 Codeeco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
