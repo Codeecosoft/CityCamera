96348776e3d0b36c7fe4e4c610ccd5dc97a991f9
Dusan Jugovic <djugovic@dusans-mac-mini.local>
2016-01-17 00:37:15 +0100

    buttons/highlight done

--- CityCamera/ViewController.swift
+++ CityCamera/ViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var buttonPhoto : UIButton!
+    @IBOutlet weak var buttonLocation : UIButton!
+    @IBOutlet weak var buttonSend : UIButton!
+    @IBOutlet weak var buttonAbout : UIButton!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonPhoto.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+        buttonLocation.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+        buttonSend.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+        buttonAbout.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
