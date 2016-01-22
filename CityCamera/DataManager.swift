935ed69a3b392a4217d26c884bcdf9f6d8823f0f
Dusan Jugovic <djugovic@dusans-mac-mini.local>
2016-01-22 18:03:14 +0100

    novi data model

--- CityCamera/DataManager.swift
+++ CityCamera/DataManager.swift
@@ -2,12 +2,15 @@
 //  DataManager.swift
 //  CityCamera
 //
-//  Created by Dusan Jugovic on 1/17/16.
+//  Created by Dusan Jugovic on 1/22/16.
 //  Copyright © 2016 Codeeco. All rights reserved.
 //
+
 import UIKit
 
 class DataManager {
     var image:UIImage? = nil
+    var longitude:String? = nil
+    var latitude:String? = nil
     static let sharedInstance = DataManager()
 }
